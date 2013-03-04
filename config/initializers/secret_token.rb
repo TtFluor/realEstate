241de336c9d0d1b03410799e28b716c2d32b0f09
Romain Pauli <gromainpauli@gmail.com>
2013-03-04 20:41:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealEstate::Application.config.secret_token = '159428f5cd1120c13fb20e834ca095e50d19ae55629cacf78583635978b1d7622698f892fafe0b13dd97295ce03cbbc333e379ce11d62da63e2be9d07cec67f2'
